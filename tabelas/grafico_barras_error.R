3213795372c650ed22fa7449a58d1d98396f6012
Guilherme Hosaka <ghosaka8@gmail.com>
2021-12-11 01:25:10 -0300

    actualizacion

--- tabelas/grafico_barras_error.R
+++ tabelas/grafico_barras_error.R
@@ -1,23 +1,23 @@
 setwd("C:/Users/ghosa/OneDrive/Documentos/GitHub/cursoR/tabelas")
 
 happiness = 
-  read.delim("tabelas/world-happiness-report-2021.csv", sep = ",")
+  read.delim("felicidad_red.csv", sep = ",")
 
-barplot(happiness$Freedom.to.make.life.choices, ylim=c(0,1), names.arg=happiness$ï..Country.name, las=2)
+barplot(happiness$Indice_felicidad, ylim=c(0,8), names.arg=happiness$Pais, las=2)
 
 regiones = c()
 
-regiones$names = unique(happiness$Regional.indicator)
+regiones$names = unique(happiness$Region)
 
 for (i in 1:length(regiones$names)){
   
   print(regiones$names[i])
-  regiones$media[i] = mean(happiness$Freedom.to.make.life.choices[happiness$Regional.indicator == regiones$names[i] ])
-  regiones$desvio[i] = sd(happiness$Freedom.to.make.life.choices[happiness$Regional.indicator == regiones$names[i] ])
+  regiones$media[i] = mean(happiness$Indice_felicidad[happiness$Region == regiones$names[i] ])
+  regiones$desvio[i] = sd(happiness$Indice_felicidad[happiness$Region == regiones$names[i] ])
   
 }
 
-x = barplot(regiones$media,beside=T,ylim=c(0,1),ylab="score", 
+x = barplot(regiones$media,beside=T,ylim=c(0,8),ylab="score", 
             names.arg = regiones$names,
             las = 2,
             col = as.factor(unique(regiones$names)))
@@ -25,4 +25,3 @@ x = barplot(regiones$media,beside=T,ylim=c(0,1),ylab="score",
 arrows(x0=x,y0=regiones$media-regiones$desvio,
        x1=x,y1=regiones$media+regiones$desvio,
        angle=90,length=0.14,code=3)
-
